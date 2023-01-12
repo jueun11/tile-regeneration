3aaf61d9c7106c3680149e7d7f753d8347816c5f
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-12 21:01:58 +0900

    리액트앱설치

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './css/App.css';
+import Time from './jsx/Time';
+
+function App() {
+  return (
+    <main>
+      <h1>Timer</h1>
+      <Time/>
+    </main>
+  );
+}
+
+export default App;
---
+++ my-app/src/jsx/Time.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Time () {
+  return(
+    <div>
+      <input id="minute" type="number"/>
+      <input id="second" type="number"/>
+    </div>
+  )
+}
+
+export default Time;
